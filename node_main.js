d5352484b524895c6ba286ba757bef4323c27c22
Portable_Maxime <maxime.lecorre96@gmail.com>
2018-02-16 15:51:53 +0100

    recuperation des restaurant sur michelin

--- node_main.js
+++ node_main.js
@@ -1,9 +1,56 @@
-var url = "https://restaurant.michelin.fr/search-restaurants?localisation=1424&cooking_type=&gm_selection=&stars=1%7C%7C2%7C%7C3&bib_gourmand=&piecette=&michelin_plate=&services=&ambiance=&booking_activated=&min_price=&max_price=&number_of_offers=&prev_localisation=1424&latitude=&longitude=&bbox_ne_lat=&bbox_ne_lon=&bbox_sw_lat=&bbox_sw_lon=&page_number=" + 0 + "&op=Rechercher&js=true"; 
-
 const request = require('request');
-request(url, function(error, response, body) {
-	console.log(body);
-});
+const cheerio = require('cheerio');
+const deasync = require("deasync")
+const request_sync = require('sync-request')
 
-  
+function recup_michelin(){
+	var url = "https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin/page-"; 
+
+	for(i = 1; i < 80 ; i++){
+	
+		console.log('traitement de la page' + i);
+
+		request(url + i, function(error, response, body) {
+		
+			$ = cheerio.load(body);
+		
+			if($('div.ds-1col').length){
+		
+				$('div.ds-1col').has('div.poi_card-display-title').each(function(i, elem) {
+				
+					var nom = $('div.poi_card-display-title',this).text();
+			
+					var tranche_prix = $('div.poi_card-display-price', this).text();
+				
+					var type_cuisine = $('div.poi_card-display-cuisines', this).text();
+				
+					var nb_avis = $('span.poi_card-display-reviews-count', this).text();
+				
+					nb_avis = nb_avis.substring(1, nb_avis.indexOf(' '));
+			
+					if($('span',this).attr('class').indexOf('1etoile') != -1){
+						var etoile = 1;
+					}
+					if($('span',this).attr('class').indexOf('2etoile') != -1){
+						var etoile = 2;
+					}
+					if($('span',this).attr('class').indexOf('3etoile') != -1){
+						var etoile = 3;
+					}
+			
+					console.log('nom du retaurant : ' + nom.trim());
+					console.log('tranche de prix : ' + tranche_prix.trim());
+					console.log('nombre d\'etoile : ' + etoile);
+					console.log('type de cuisine : ' + type_cuisine.trim());
+					console.log('nombre d\'avis : ' + nb_avis); 
+					console.log('');
+				})
+			}
+		});
+	
+	}
+}
+
+recup_michelin();
+
   
