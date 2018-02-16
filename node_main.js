caa533603259f30ccf7da7aa4c401d66e016bd42
Portable_Maxime <maxime.lecorre96@gmail.com>
2018-02-16 19:16:13 +0100

    ajout d'inforrmation pour les restaurants michelin

--- node_main.js
+++ node_main.js
@@ -7,17 +7,46 @@ function recup_michelin(){
 	var url = "https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin/page-"; 
 
 	for(i = 1; i < 80 ; i++){
-	
+		
 		console.log('traitement de la page' + i);
 
 		request(url + i, function(error, response, body) {
 		
 			$ = cheerio.load(body);
-		
+			
+			//var json = $("script:contains('jQuery.extend')").val();
+			
+			//console.log(json);
+			
 			if($('div.ds-1col').length){
-		
+				
 				$('div.ds-1col').has('div.poi_card-display-title').each(function(i, elem) {
 				
+					var arrondi_note = -1;
+				
+					$('li',this).each(function(i, elem) {
+						
+						switch($(this).attr('class').substring(0,1)){
+							case 'O':
+								if(arrondi_note == -1){arrondi_note = 0;}
+								arrondi_note++;
+								break;
+							
+							case 'D':
+								if(arrondi_note == -1){arrondi_note = 0;}
+								arrondi_note += 0.5;
+								break;
+								
+							case 'N':
+								if(arrondi_note == -1){arrondi_note = 0;}
+								arrondi_note += 0;
+								break;
+								
+							default:
+								break;
+						}
+					})
+					
 					var nom = $('div.poi_card-display-title',this).text();
 			
 					var tranche_prix = $('div.poi_card-display-price', this).text();
@@ -26,8 +55,10 @@ function recup_michelin(){
 				
 					var nb_avis = $('span.poi_card-display-reviews-count', this).text();
 				
-					nb_avis = nb_avis.substring(1, nb_avis.indexOf(' '));
-			
+					nb_avis = nb_avis.substring(0, nb_avis.indexOf(' '));
+					
+					if (nb_avis == ''){nb_avis = 0;}
+					
 					if($('span',this).attr('class').indexOf('1etoile') != -1){
 						var etoile = 1;
 					}
@@ -43,6 +74,8 @@ function recup_michelin(){
 					console.log('nombre d\'etoile : ' + etoile);
 					console.log('type de cuisine : ' + type_cuisine.trim());
 					console.log('nombre d\'avis : ' + nb_avis); 
+					console.log('note : ' + arrondi_note);
+					
 					console.log('');
 				})
 			}
