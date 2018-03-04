4fde7404f31b40d21155058a85e7f88745d128fe
Portable_Maxime <maxime.lecorre96@gmail.com>
2018-03-04 22:02:04 +0100

    suppresion des fichiers inutiles

--- public/restaurant-request.js
+++
@@ -1,1 +0,0 @@
-https://restaurant.michelin.fr/search-restaurants?localisation=1424&cooking_type=&gm_selection=&stars=1%7C%7C2%7C%7C3&bib_gourmand=&piecette=&michelin_plate=&services=&ambiance=&booking_activated=&min_price=&max_price=&number_of_offers=&prev_localisation=1424&latitude=&longitude=&bbox_ne_lat=&bbox_ne_lon=&bbox_sw_lat=&bbox_sw_lon=&page_number=0&op=Rechercher&js=true
